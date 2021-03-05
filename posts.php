4d689020fd9ca6c94012a12dad169eeea1bf8169
djeday <gvozdetskyidev@gmail.com>
2021-03-05 11:50:59 +0200

    LT-4 Posts page layout

---
+++ posts.php
@@ -0,0 +1,54 @@
+<!DOCTYPE  html>
+<html lang="ru">
+<head>
+    <title>Posts list</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style-posts.css">
+</head>
+<body>
+<header>
+    <div class="header-img">
+        <img src="images/logo_welcome.png" alt="site logo"/>
+    </div>
+</header>
+<main>
+    <h1 class="main-title">Список статей</h1>
+    <div class="posts-list">
+        <ul>
+            <li>
+                <article>
+                    <div class="post-info">
+                        <a href="">Redmal</a><span class="post-time">04.03.2021 в 12:20</span>
+                    </div>
+                    <h2 class="post-title">Как ML помогает при аудите качества клиентского сервиса</h2>
+                    <div class="post-image">
+                        <img src="https://habrastorage.org/getpro/habr/upload_files/649/f65/762/649f65762c9102c5a7ac8b51f50f3dcf.jpg" alt="post image">
+                    </div>
+                    <div class="post-text">
+                        <p>Можно ли за короткое время и без больших трудозатрат проанализировать обращения клиентов и выявить причины возникновения негативных отзывов? В этой статье хотим рассказать, как с помощью инструментов ML нам удалось решить эту задачу.</p>
+                    </div>
+                </article>
+            </li>
+            <li>
+                <article>
+                    <div class="post-info">
+                        <a href="">Redmal</a><span class="post-time">04.03.2021 в 11:20</span>
+                    </div>
+                    <h2 class="post-title">Что такое yield и как он работает в C#?</h2>
+                    <div class="post-image">
+                        <img src="https://habrastorage.org/getpro/habr/upload_files/29a/1c0/476/29a1c0476809e4f2624d514ce893fbb3.png" alt="post image">
+                    </div>
+                    <div class="post-text">
+                        <p>Возможности C# из года в год становятся всё шире. Разные фичи делают жизнь программиста приятнее, но предназначение и особенности некоторых из них могут быть очевидны не всем. Например, старый-добрый yield. Для некоторых разработчиков, особенно начинающих, это самая настоящая магия – непонятная, но интересная. В данной статье будет показано, как же всё-таки работает yield, и что на самом деле скрыто за этим волшебным словом. Приятного чтения!</p>
+                    </div>
+                </article>
+            </li>
+        </ul>
+    </div>
+</main>
+<footer>
+    &copy; Гвоздецкий Вячеслав
+</footer>
+
+</body>
+</html>
